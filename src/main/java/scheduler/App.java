eff2f2dbe18527d392f48b0888ad8507edb5213b
Pavle Koljancic <pavle.koljancic@student.etf.unibl.org>
2023-05-02 13:43:25 +0200

    Dodao sam scheduled Algoritme za scheduler i napravio incijalnu verziju taska.

--- src/main/java/scheduler/App.java
+++ src/main/java/scheduler/App.java
@@ -1,13 +1,21 @@
 package scheduler;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 /**
  * Hello world!
  *
  */
-public class App 
-{
+import java.util.HashSet;
+import java.util.Scanner;
+
+import scheduler.Task.Task;
+public class App  
+{   
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+      
     }
+
 }
+
